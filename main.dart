b83f3d5bca5d395aec6d2febc5b063cf86ef8c6f
dlworua <leegaegyum@gmail.com>
2025-03-27 22:52:50 +0900

    feat: 마이페이지에 사용할 state클래스 정의

--- main.dart
+++ main.dart
@@ -21,3 +21,19 @@ class MyHomePage extends StatefulWidget {
     throw UnimplementedError();
   }
 }
+
+//마이페이지에서 사용할 state클래스 정의
+class MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Test app')),
+      body: Center(child: Text('$counter')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
